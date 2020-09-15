c7c8d16d754868af3138e35432542c35aaadb975
Tyler Davis <tylerdavis@Tylers-iMac-2.lan1>
2020-09-15 07:44:48 -0400

    Finsihed building the custom text input component.

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import Screen from "./app/components/Screen";
 import { TextInput, Text } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
-
-  return <Screen></Screen>;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Platform } from "react-native";
 
 import styles from "./styles";
 
 export default function AppText({ children, style }) {
   return <Text style={[styles.text, style]}>{children}</Text>;
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
